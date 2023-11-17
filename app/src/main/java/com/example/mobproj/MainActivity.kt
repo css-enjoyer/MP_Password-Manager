a5c3e775fbfcf2effdc00c0471dcc0283c5c48a5
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-11-17 14:42:11 +0800

    Add creation modals and lockscreen

--- app/src/main/java/com/example/mobproj/MainActivity.kt
+++ app/src/main/java/com/example/mobproj/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.mobproj
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -11,26 +16,128 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fabAdd: FloatingActionButton
     private lateinit var menuItems: LinearLayout
 
+    // For initial lockscreen
+    private lateinit var pinDialog: Dialog
+    private lateinit var pinInput: EditText
+    private lateinit var confirmButton: Button
+    private lateinit var instructionText: TextView
+    private var pinAttempts = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initPinDialog()
+        showPinDialog()
+
         fabAdd = findViewById(R.id.fabAdd)
         menuItems = findViewById(R.id.menuItems)
-// Handle Add Click
+
+        // Handle Add Button Click
         fabAdd.setOnClickListener {
             menuItems.visibility = if (menuItems.visibility == View.VISIBLE) View.GONE else View.VISIBLE
         }
     }
 
-    // Handle Folder Click
-    fun onFolderClick(view: View) {
-        Toast.makeText(this, "Folder selected", Toast.LENGTH_SHORT).show()
+    private fun initPinDialog() {
+        pinDialog = Dialog(this)
+        pinDialog.setContentView(R.layout.lockscreen)
+
+        // Initialize UI components from the dialog
+        pinInput = pinDialog.findViewById(R.id.pinInput)
+        instructionText = pinDialog.findViewById(R.id.instructionText)
+        confirmButton = pinDialog.findViewById(R.id.confirmButton)
+
+        // Set click listener for the confirm button
+        confirmButton.setOnClickListener {
+            // Check the entered PIN
+            checkPin(pinInput.text.toString())
+        }
+    }
+    private fun showPinDialog() {
+        // Show the PIN dialog
+        pinDialog.show()
+    }
+    private fun checkPin(enteredPin: String) {
+        // Default PIN
+        val defaultPin = "0000"
+
+        // Check if the entered PIN is correct
+        if (enteredPin == defaultPin) {
+            pinDialog.dismiss()
+        } else {
+            // Incorrect PIN, increment attempts
+            pinAttempts++
+            // Clear PIN input
+            pinInput.text.clear()
+            // Update instruction text
+            instructionText.text = "Incorrect PIN. Attempts: $pinAttempts"
+            // Add logic for handling incorrect PIN attempts (e.g., show a message after a certain number of attempts)
+            if (pinAttempts >= 3) {
+                Toast.makeText(this, "Three attempts reached.", Toast.LENGTH_SHORT).show()
+                pinAttempts = 0
+
+            }
+        }
     }
 
-    // Handle Password Click
+    // Handle Folder Button Click
+    fun onFolderClick(view: View) {
+        // Inflate the Folder_Creation_Layout
+        val folderCreationView = layoutInflater.inflate(R.layout.folder_creation_layout, null)
+
+        // Initialize UI components
+        val folderTitle: EditText = folderCreationView.findViewById(R.id.folderTitle)
+        val folderDescription: EditText = folderCreationView.findViewById(R.id.folderDescription)
+        val folderColor: EditText = folderCreationView.findViewById(R.id.folderColor)
+        val btnSave: Button = folderCreationView.findViewById(R.id.btnSave)
+        val btnCancel: Button = folderCreationView.findViewById(R.id.btnCancel)
+
+        // Create AlertDialog
+        val folderCreationDialog = AlertDialog.Builder(this)
+            .setView(folderCreationView)
+            .create()
+
+        // Pass inputs to database on save
+        btnSave.setOnClickListener {
+            val title = folderTitle.text.toString()
+            val description = folderDescription.text.toString()
+            val color = folderColor.text.toString()
+            folderCreationDialog.dismiss()
+        }
+        btnCancel.setOnClickListener {
+            folderCreationDialog.dismiss()
+        }
+        folderCreationDialog.show()
+    }
+
+    // Handle Password Button Click
     fun onPasswordClick(view: View) {
-        Toast.makeText(this, "Password selected", Toast.LENGTH_SHORT).show()
+        // Inflate Password_Creation_Layout
+        val passwordCreationView = layoutInflater.inflate(R.layout.password_creation_layout, null)
+        // Initialize UI components
+        val passwordTitle: EditText = passwordCreationView.findViewById(R.id.passwordTitle)
+        val passwordInput: EditText = passwordCreationView.findViewById(R.id.passwordInput)
+        val passwordDescription: EditText = passwordCreationView.findViewById(R.id.passwordDescription)
+        val btnSave: Button = passwordCreationView.findViewById(R.id.btnSave)
+        val btnCancel: Button = passwordCreationView.findViewById(R.id.btnCancel)
+
+        // Create AlertDialog
+        val passwordCreationDialog = AlertDialog.Builder(this)
+            .setView(passwordCreationView)
+            .create()
+
+        // Pass inputs to database on save
+        btnSave.setOnClickListener {
+            val title = passwordTitle.text.toString()
+            val pw = passwordInput.text.toString()
+            val description = passwordDescription.text.toString()
+            passwordCreationDialog.dismiss()
+        }
+        btnCancel.setOnClickListener {
+            passwordCreationDialog.dismiss()
+        }
+        passwordCreationDialog.show()
     }
 }
 
